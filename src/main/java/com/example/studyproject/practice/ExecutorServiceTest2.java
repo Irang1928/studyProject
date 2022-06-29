6b621d71112fc422b734d7ec89b0a9731fb4ce26
Irang <yupk1928@naver.com>
2022-06-29 15:05:38 +0900

    Executor 예제

---
+++ src/main/java/com/example/studyproject/practice/ExecutorServiceTest2.java
@@ -0,0 +1,31 @@
+package com.example.studyproject.practice;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorServiceTest2 {
+    public static void main(String args[]) throws InterruptedException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(() -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Job1 " + threadName);
+        });
+        executor.submit(() -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Job2 " + threadName);
+        });
+        executor.submit(() -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Job3 " + threadName);
+        });
+        executor.submit(() -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Job4 " + threadName);
+        });
+
+        executor.shutdown();
+        executor.awaitTermination(20, TimeUnit.SECONDS);
+        System.out.println("end");
+    }
+}
